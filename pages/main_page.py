5c64c43653904c75bcf92955b943c7f81208cb67
Albina <albina.pvp@mail.ru>
2023-10-18 14:33:35 +0500

    Preparing files using Page Object

---
+++ pages/main_page.py
@@ -0,0 +1,8 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+#создайте класс  MainPage. Его нужно сделать наследником класса BasePage. Класс-предок в Python указывается в скобках:
+class MainPage(BasePage):
+    #Чтобы все работало, надо слегка видоизменить его. В аргументы больше не надо передавать экземпляр браузера, мы его передаем и сохраняем на этапе создания Page Object. Вместо него нужно указать аргумент self , чтобы иметь доступ к атрибутам и методам класса
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
