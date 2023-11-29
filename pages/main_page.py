de94234a1c214105270ba898c2d5ca1cc3461ff8
Albina <albina.pvp@mail.ru>
2023-11-29 16:18:10 +0500

    add chech forms

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,22 @@
+from .locators import MainPageLocators
+from .locators import LoginPageLocators
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 #создайте класс  MainPage. Его нужно сделать наследником класса BasePage. Класс-предок в Python указывается в скобках:
 class MainPage(BasePage):
     #Чтобы все работало, надо слегка видоизменить его. В аргументы больше не надо передавать экземпляр браузера, мы его передаем и сохраняем на этапе создания Page Object. Вместо него нужно указать аргумент self , чтобы иметь доступ к атрибутам и методам класса
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
     #Реализуем метод, который будет проверять наличие ссылки
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    #Реализуем метод, который будет проверять наличие login в текущем url браузера
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "'login' is not in current_url from browser"
+    #Реализуем метод, который будет проверять наличие формы логина
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+    #Реализуем метод, который будет проверять наличие формы регистрации
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -8,3 +8,6 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     page.should_be_login_link()      # выполняем метод, который будет проверять наличие ссылки
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
